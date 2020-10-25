6b8461ec11f372f1b376a619d9bea137c87e4278
JuditBarabas <62601033+JuditBarabas@users.noreply.github.com>
2020-10-25 11:29:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('For solution open js file');
+
+//prewritten
+const leaderboard = ['Harry', 'Lua', 'Hermoine', 'Bellatrix'];
+
+// YOUR CODE BELOW HERE:
+leaderboard[1] = 'Luna';
+leaderboard[3] = 'Draco';
